838e322ac6f37f29a73fdce6c68b3b39d9829a89
dayang4321 <dayang4321@users.noreply.github.com>
2020-06-23 09:14:06 +0100

    build

---
+++ build/precache-manifest.0e61ba330925c74a3dbe7be0f5d1fe78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57c80978bcb38803ab6c9caac77caad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa0ba91224b51a7ae85",
+    "url": "/static/css/main.8969d816.chunk.css"
+  },
+  {
+    "revision": "4ae8c4723cc706e0c51c",
+    "url": "/static/js/2.7a658827.chunk.js"
+  },
+  {
+    "revision": "aab86b904eb578eea9b3d8e43f66c7b0",
+    "url": "/static/js/2.7a658827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa0ba91224b51a7ae85",
+    "url": "/static/js/main.e3f626f7.chunk.js"
+  },
+  {
+    "revision": "4639bc511991fef86236",
+    "url": "/static/js/runtime-main.28fda31b.js"
+  }
+]);
