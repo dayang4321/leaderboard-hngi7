12ab3ea40eac93e728cd983c6cbfc12a5dacbb6a
dayang4321 <dayang4321@users.noreply.github.com>
2020-06-23 12:24:10 +0100

    Updates

--- precache-manifest.1613d81543838f64ea9925ed2bbf4ef3.js
+++ precache-manifest.1613d81543838f64ea9925ed2bbf4ef3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c0c3ec48d159642d613d5e68f91a82f",
+    "revision": "c2c73fbc130c77758f5b06abf4326180",
     "url": "/leaderboard-hngi7/index.html"
   },
   {
-    "revision": "de89ee82f7dc862e1f6b",
+    "revision": "c7ff2d02675a99d81ba7",
     "url": "/leaderboard-hngi7/static/css/main.8969d816.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/leaderboard-hngi7/static/js/2.0231531b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "de89ee82f7dc862e1f6b",
-    "url": "/leaderboard-hngi7/static/js/main.0fc1ab44.chunk.js"
+    "revision": "c7ff2d02675a99d81ba7",
+    "url": "/leaderboard-hngi7/static/js/main.1b093041.chunk.js"
   },
   {
     "revision": "5535ff445159f06d36ac",
